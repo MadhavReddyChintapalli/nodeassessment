4c3ee02c3251fd8698e6308284ff279d0e0d43f9
Madhav Reddy Chintapalli <chintapallimadhav@gmail.com>
2022-01-14 17:50:37 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const getCombinations = arr => {
+  const output = [];
+
+  const swap = (arrToSwap, indexA, indexB) => {
+    const temp = arrToSwap[indexA];
+    arrToSwap[indexA] = arrToSwap[indexB];
+    arrToSwap[indexB] = temp;
+  };
+
+  const generate = (n, heapArr) => {
+    if (n === 1) {
+      output.push(heapArr.slice());
+      return;
+    }
+
+    generate(n - 1, heapArr);
+
+    for (let i = 0; i < n - 1; i++) {
+      if (n % 2 === 0) {
+        swap(heapArr, i, n - 1);
+      } else {
+        swap(heapArr, 0, n - 1);
+      }
+
+      generate(n - 1, heapArr);
+    }
+  };
+
+  generate(arr.length, arr.slice());
+
+  return output;
+};
+
+console.log(getCombinations(['a', 'b', 'c', 'd']));
